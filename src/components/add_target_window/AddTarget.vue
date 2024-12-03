72c7511d0a4843a18c0914fdfc7d7d106ea9a33a
oywq2020 <2403_87019412@noreply.gitcode.com>
2024-12-03 18:58:05 +0800

    complete the add target panel css

--- src/components/add_target_window/AddTarget.vue
+++ src/components/add_target_window/AddTarget.vue
@@ -1,16 +1,29 @@
 <template>
-  <div v-if="visible" class="modal-overlay">
+  <div v-show="visible" class="modal-overlay">
     <div class="modal-content">
       <div class="header">
         <span>添加任务</span>
         <button @click="closeAddTargetWin">关闭</button>
+      </div>
+
+      <div class="input-content">
+        <textarea></textarea>
+        <div>
+          <input id="date_input" ref="datePick" type="date"/>
+          <input id="hours_input" type="number" />
+          <input id="minutes_input" type="number" />
+        </div>
+        <button class="certify_button">确定</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { onMounted } from "vue";
+
 export default {
+  setup() {},
   data() {
     return {
       isDrag: false,
@@ -23,6 +36,9 @@ export default {
     openAddTargetWin() {
       console.log("openAddTargetWin");
       this.visible = true;
+
+      //init controls 
+      this.$refs.datePick.value = new Date().toISOString().split("T")[0]; 
     },
     closeAddTargetWin() {
       this.visible = false;
@@ -33,6 +49,8 @@ export default {
       this.closeModal();
     },
   },
+
+  //lifetime function
 };
 </script>
 <style scoped>
@@ -72,8 +90,55 @@ export default {
   font-size: 18px;
   font-family: DingTalkJinBuTi;
   grid-column: 2;
+  user-select: none;
 }
 .header button {
   grid-column: 3;
 }
+
+.input-content {
+  width: 432px;
+  height: 205px;
+  display: grid;
+  grid-template-rows: 3fr 1fr 1fr;
+  justify-items: center;
+}
+.input-content textarea {
+  grid-row: 1;
+  height: 100px;
+  width: 300px;
+  margin-top: 20px;
+  resize: none;
+}
+.input-content div {
+  grid-row: 2;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+}
+.input-content #date_input {
+  width: 80px;
+  height: 16px;
+  font-size: 9.5px;
+  
+}
+.input-content #hours_input {
+  width: 40px;
+  height: 16px;
+  font-size: 9.5px;
+  text-align: center;
+}
+.input-content #minutes_input {
+  width: 40px;
+  height: 16px;
+  font-size: 9.5px;
+  text-align: center;
+}
+.certify_button {
+  width: 60px;
+  height: 28px;
+  grid-row: 3;
+  }
 </style>
